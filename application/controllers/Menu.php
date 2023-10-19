198f4963b9f7ec41dee7e40cf747e43ed560db12
Ramadhangalang <ramadhangalang1702@gmail.com>
2023-10-19 21:33:46 +0800

    pertemuan 1

---
+++ application/controllers/Menu.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Menu extends CI_Controller
+{
+
+
+    public function index()
+    {
+        $this->load->view('template/menu');
+    }
+}
---
+++ application/views/template/main.php
@@ -0,0 +1,6 @@
+<?php
+$this->load->Views('template/header');
+$this->load->Views('template/navbar');
+$this->load->Views('template/sidebar');
+$this->load->Views($content);
+$this->load->Views('template/footer');
